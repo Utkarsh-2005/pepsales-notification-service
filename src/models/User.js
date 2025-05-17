3da6aceee7fdfed6386bba8bd2351151022a480b
Utkarsh <utkarshjha.4009@gmail.com>
2025-05-18 02:42:25 +0530

    Removed phoneno from model

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,6 @@ const userSchema = new mongoose.Schema({
   uid:       { type: String, required: true, unique: true },
   name:      { type: String, required: true },
   email:     { type: String, required: true, unique: true },
-  phone:     { type: String, unique: true },
   createdAt: { type: Date,   default: Date.now }
 })
 
